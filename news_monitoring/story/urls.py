07079c21213232aa88d10ce67576733138e47abc
VaibhavSingh2004 <vaibhavsinghcse2004@gmail.com>
2025-04-02 16:40:16 +0530

    add ajax call for search feature in story listing page

--- news_monitoring/story/urls.py
+++ news_monitoring/story/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from ..source.urls import app_name
-from .views import add_or_edit, delete
+from .views import add_or_edit, delete, autocomplete_stories, search_stories
 from .views import list
 
 app_name = "story"
@@ -10,4 +10,6 @@ urlpatterns = [
     path("list/", list, name="list"),
     path("edit/<int:story_id>/", add_or_edit, name="edit"),
     path("delete/<int:story_id>/", delete, name="delete"),
+    path('search/', search_stories, name='search'),
+    path('autocomplete/', autocomplete_stories, name='autocomplete'),
 ]
