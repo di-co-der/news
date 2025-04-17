88a74f19507da9d98086162886782c1ec0bcea6e
di-co-der <raghavdivyansh9@gmail.com>
2025-04-17 09:14:33 +0530

    changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -244,7 +244,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Vaibhav Singh""", "vintageubuntu@gmail.com")]
+ADMINS = [("""Divyansh Raghav""", "vintageubuntu@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
